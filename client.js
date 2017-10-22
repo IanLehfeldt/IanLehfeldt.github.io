5883f9bc2cc2b6466f20a8e21d3c29129ec90327
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2017-10-22 11:15:15 -0500

    sourced in bootstrap, angularjs and client scripts

---
+++ client.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp', []);
+
+// console.log('My app client loaded');
+
+myApp.controller('PageController', function () {
+    // console.log('Page Controller is loaded');
+    
+});
