80fbeb06cff01a35d18041040b7126888637cbdc
Ian Lehfeldt <ian.lehfeldt@gmail.com>
2018-01-10 11:17:35 -0600

    did some styling fixes after viewing on a diff browser and added in a palindrome project!

--- scripts/client.js
+++ scripts/client.js
@@ -1,8 +1,39 @@
-var myApp = angular.module('myApp', []);
+$(document).ready(function () {
+    console.log('jQuery loaded');
 
-// console.log('My app client loaded');
 
-myApp.controller('PageController', function () {
-    // console.log('Page Controller is loaded');
-    
+    $('#palindromeButton').on('click', function () {
+        $('#message').empty();
+
+        let palindromeInput = $('#inputText').val();
+        // console.log(`palindrome button clicked and the value is ${palindromeInput}`);
+        if (palindromeInput === "") {
+            $('#message').append('<p>Please input a sentence</p>')
+        } else {
+            if (isItAPalindrome(palindromeInput)) {
+                $('#message').append('<p>Success! ' + palindromeInput + ' is a palindrome.</p>')
+            } else {
+                $('#message').append('<p>Sorry! "' + palindromeInput + '" is not a palindrome.</p>')
+            }
+        }
+    });
+
+    isItAPalindrome = (sen) => {
+        var palindromeToCheck = sen.replace(/[.,+ !-]/g, "").toLowerCase();
+        var reversedSentence = "";
+
+        // console.log(`Sen: ${sen}, ${sen.length} : palindromeToCheck: ${palindromeToCheck}`);
+
+        for (var i = palindromeToCheck.length - 1; i >= 0; i--) {
+            //console.log(`Sen at ${i} ${palindromeToCheck[i]}`);
+            reversedSentence += (palindromeToCheck[i]);
+        }
+
+        // console.log("Reversed array:", reversedSentence);
+        if (palindromeToCheck === reversedSentence) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 });
